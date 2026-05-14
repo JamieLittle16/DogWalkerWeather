61daabe13275843138cb10cfd546cb6fe4f3875f
Lawrence Blackbourne <lawrenceblackbourne1@outlook.com>
2026-05-14 15:57:56 +0100

    Fixed Data Storage

--- src/main/java/cambridge/weatherapp/dogwalkerweather/model/WeatherData.java
+++ src/main/java/cambridge/weatherapp/dogwalkerweather/model/WeatherData.java
@@ -1,24 +1,31 @@
 package cambridge.weatherapp.dogwalkerweather.model;
 
-import java.util.List;
+import java.util.ArrayList;
 
 // This class holds the current weather data
 // The object will be returned by api for a specific query
 public class WeatherData {
-    private double temperature;
-    private double humidity;
     private String groundCondition;
-    private List<HourlyForecast> dailyForecast;
+    private ArrayList<HourlyForecast> dailyForecast;
 
-    public WeatherData(double temperature, double humidity, String groundCondition, List<HourlyForecast> dailyForecast) {
-        this.temperature = temperature;
-        this.humidity = humidity;
+    public WeatherData(String groundCondition, ArrayList<HourlyForecast> dailyForecast) {
         this.groundCondition = groundCondition;
         this.dailyForecast = dailyForecast;
     }
 
-    public double getTemperature() { return temperature; }
-    public double getHumidity() { return humidity; }
     public String getGroundCondition() { return groundCondition; }
-    public List<HourlyForecast> getDailyForecast() { return dailyForecast; }
+    public ArrayList<HourlyForecast> getDailyForecast() { return dailyForecast; }
+
+    public double getCurrentTemperature() { return dailyForecast.get(0).getTemperature(); }
+    public double getCurrentFeelsLikeTemperature() { return dailyForecast.get(0).getFeelsLikeTemperature(); }
+    public double getCurrentWindSpeed() { return dailyForecast.get(0).getWindSpeed(); }
+    public double getCurrentWindDirection() { return dailyForecast.get(0).getWindDirection(); }
+    public double getCurrentWindGustSpeed() { return dailyForecast.get(0).getWindGustSpeed(); }
+    public double getCurrentVisibility() { return dailyForecast.get(0).getVisibility(); }
+    public double getCurrentRelativeHumidity() { return dailyForecast.get(0).getRelativeHumidity(); }
+    public double getCurrentPressure() { return dailyForecast.get(0).getPressure(); }
+    public double getCurrentUV() { return dailyForecast.get(0).getUV(); }
+    public double getCurrentDewPoint() { return dailyForecast.get(0).getDewPoint(); }
+    public int getCurrentWeatherCode() { return dailyForecast.get(0).getWeatherCode(); }
+    public int getCurrentPrecipitationProbability() { return dailyForecast.get(0).getPrecipitationProbability(); }
 }
