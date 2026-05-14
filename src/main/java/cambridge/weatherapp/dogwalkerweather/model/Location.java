04f0adf485feb4125baf362f6f6edf4e0017a509
Leo Zhang <leozhang0214@gmail.com>
2026-05-14 16:14:19 +0100

    Start weather API integration

--- src/main/java/cambridge/weatherapp/dogwalkerweather/model/Location.java
+++ src/main/java/cambridge/weatherapp/dogwalkerweather/model/Location.java
@@ -3,24 +3,31 @@ package cambridge.weatherapp.dogwalkerweather.model;
 // Other locations need to be added,
 // Pairs location with site ID, think they're correct but check
 public enum Location {
-    CAMBRIDGE("Cambridge", "310042"),
-    LONDON("London", "350661"),
-    OSLO("Oslo", "123456"); // Don't think MET Support none UK
+    CAMBRIDGE("Cambridge", 52.1988349,0.0787878),
+    LONDON("London", 51.5285262,-0.2664037),
+    OSLO("Oslo", 64.1820777,7.2190473);
 
     private final String displayName;
-    private final String apiId;
+    private final Double latitude;
+    private final Double longitude;
 
     // Location Enum Constructor
-    Location(String displayName, String apiId) {
+    Location(String displayName, Double latitude, Double longitude) {
         this.displayName = displayName;
-        this.apiId = apiId;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getDisplayName() {
         return displayName;
     }
 
-    public String getApiId() {
-        return apiId;
+    public Double getLatitude() {
+        return latitude;
     }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
 }
